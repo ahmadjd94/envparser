7afe0878ccc54f3aa269ed6578e90224517afb45
banana <ahmadjd94@gmail.com>
2018-03-24 20:59:42 +0200

    add script file

---
+++ loader.py
@@ -0,0 +1,21 @@
+"""Author: Ahmad Da'na
+import this function and provide the path the your .env file """
+
+import os
+import re
+
+
+def load(path=""):
+	"""
+	load environment variables from text file
+	:type path: str
+	:param path: path your .env file
+	:return: None
+	"""
+	with open(path, 'r') as fil:
+		for i in fil:
+			if re.match("#.*|\n", i):
+				continue
+			line = re.sub("""\s|\n|\t|\'|\"""", "", i)
+			line = line.split("=")
+			os.environ[line[0]] = line[1]
